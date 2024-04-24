f368fc23d9ac7eb1ef033c0c3bcdf11c2856a07c
SilvieFlidrova <flidrova@spsostrov.cz>
2024-04-24 16:10:24 +0200

    třikrát stejná zpráva

---
+++ Domaci_ukoly/Ukoly_lekce_4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ukoly_lekce_4
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ukoly z lekce4!");
+
+            //1) Pomocí cyklu vypiš 3x nějakou stejnou zprávu
+           
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Ahoj, přeji Ti krásný den a samé šikovné studenty");
+            }
+
+
+          
+            Console.ReadLine();
+            Console.WriteLine("konec programu");
+        }
+    }
+}
+      
+       
+
+
+               
+
